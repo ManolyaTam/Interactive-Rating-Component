bc62b6c8186f39af07d839347d91b181e1f54788
Manolya <201097@ppu.edu.ps>
2022-07-25 07:57:15 +0300

    buttons rating done, and result of rating shows ... i guess i am done

--- app.js
+++ app.js
@@ -2,13 +2,63 @@ const submitBtn = document.getElementById('submitBtn');
 const page1 = document.getElementById('page1');
 const page2 = document.getElementById('page2');
 const result = document.getElementById('result');
+const btns = document.getElementById('btns');
+let rate = 0;
+/**
+ * @param {Array <{value : Number, selected : boolean}[]>}
+ */
+const buttons = [
+    {
+        value: 1,
+        selected: false
+    },
+    {
+        value: 2,
+        selected: false
+    },
+    {
+        value: 3,
+        selected: false
+    },
+    {
+        value: 4,
+        selected: false
+    },
+    {
+        value: 5,
+        selected: false
+    }
+]
 
-let rating = 0;
+selectRate = (index) => {
+    clearSelection();
+    buttons[index].selected = true;
+    rate = index + 1;
+    renderBtns();
+}
+
+renderBtns = () => {
+    btns.innerHTML = '';
+    for (let i = 0; i < 5; i += 1) {
+        let btn = buttons[i];
+        btns.innerHTML += `
+        <button onclick="selectRate(${i})" class="btn ${btn.selected ? 'clicked' : ''}">${btn.value}</button>
+    `
+    }
+}
+renderBtns();
+
+clearSelection = () => {
+    for (let i = 0; i < 5; i += 1) {
+        let btn = buttons[i];
+        btn.selected = false;
+    }
+    renderBtns();
+}
 
 submitBtn.onclick = () => {
     page1.style.display = 'none';
-
-    result.innerHTML = `You selected ${rating} out of 5`
+    result.innerHTML = `You selected ${rate} out of 5`
     page2.style.display = 'flex';
 
 }
